f31da40439540095edfa62213881cdbaad2ccc61
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-12-22 00:01:51 +0200

    Training Bahdanau Softmax on the Augmented Dataset

---
+++ richness_metrics.py
@@ -0,0 +1,13 @@
+from collections import Counter
+from nltk.util import ngrams 
+
+text = ""
+n_gram = 1
+
+with open("../L101_metrics/my_metrics/11/sampled_ref.txt", "r") as f:
+	for line in f:
+		line = line.strip("\n").lower()
+		text += line
+
+	print(len(list(Counter(ngrams(text.split(), n_gram)))))
+
